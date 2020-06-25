d22f056000818b3f11debd6a4c88285b4e158495
thejavierCO <javier.cruz.odispo@hotmail.com>
2020-06-25 15:57:59 -0500

    organiced

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-const api = require("./territorum");
-const morgan = require("morgan");
-const engine = require("ejs-mate");
 const express = require("express");
-let tr = new api("https://conalep.territorio.la/");
-let app = express();
-app.engine("ejs",engine);
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true}))
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(morgan("dev"));
-app.set('port',process.env.PORT||8080)
-
-// app.get("/token/:user/:pass",({params},res,next)=>{
-//     tr.auth(params.user,params.pass).then(({cookie})=>{
-//         res.json(cookie);
-//     })
-// })
-
-// app.post("/posts",({body:{cookie={}}},res,next)=>{
-//     tr.getPosts({c:0,i:20},cookie).then(e=>{
-//         console.log(e.cookie)
-//         res.send(e.data);
-//     })
-// })
+app.engine("ejs",require("ejs-mate"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('port',process.env.PORT||3000);
 
 app.get("/",(req,res,next)=>{
-    res.json({});
-})
+    res.render("main");
+});
 
-app.listen(app.get('port'),()=>{console.log('run',app.get('port'))})
+app.use("/files",express.static(__dirname+"/public"));
+
+app.listen(app.get('port'),()=>{console.log('run',app.get('port'))});
