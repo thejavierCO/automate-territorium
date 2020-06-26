097847e196b8730ec15148f56a29dbd8ff63cdc5
thejavierCO <javier.cruz.odispo@hotmail.com>
2020-06-26 02:07:45 -0500

    dia 1

--- src/components/territorio.js
+++ src/components/territorio.js
@@ -4,5 +4,28 @@ const tough = require('tough-cookie');axiosCookieJarSupport(axios);
 const {CookieJar,Cookie} = tough;
 const cookieJar = new CookieJar;
 const cheerio = require('cheerio');
+const urlcode = require("form-urlencoded");
 
-module.exports = {};
+class api{
+    constructor(url){this.url = url;}
+    call = (method="",{url=this.url,path="/",params=""},cookie=cookieJar)=>axios[method](
+            url+path,params,{
+            jar:cookie,
+            withCredentials: true
+        })
+    .then(({data,headers})=>({
+        data: cheerio.load(data),
+        headers,
+        cookie,
+        router:{url,path}}
+    ))
+    .catch(a=>{
+        a.url = url;
+        return a;
+    })
+    getSession = ()=>this.call("get").then(({cookie})=>cookie);
+    getAuth = (params)=>this.call("post",{},urlcode(params))
+    test = (params)=>urlcode(params);
+}
+
+module.exports = api;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const api = require("./components/territorio")
 const app = express();
 
+const tr = new api("http://localhost:3000");
+
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(morgan("dev"));
@@ -10,11 +15,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port',process.env.PORT||3000);
 
+app.use("/files",express.static(__dirname+"/public"));
+
 app.use("/",({body:{cookie=[]},headers},res,next)=>{
     console.log(cookie,headers);
-    res.render("main");
+    res.render("main",{string:tr.test({
+        "play":"ijaosjd"
+    })});
 });
-
-app.use("/files",express.static(__dirname+"/public"));
 
 app.listen(app.get('port'),()=>{console.log('run',app.get('port'))});
