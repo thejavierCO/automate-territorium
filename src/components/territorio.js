8ef79ef7765b09322b96a9346ece21cd3fbddc41
thejavierCO <javier.cruz.odispo@hotmail.com>
2020-06-27 20:07:51 -0500

    yes send info

--- src/components/territorio.js
+++ src/components/territorio.js
@@ -8,7 +8,7 @@ const cheerio = require('cheerio');
 
 const getSI = (type="")=>
 type==="auth"?
-    ((user,password,login="Iniciar%20Sesi%C3%B3n",email)=>"MyUserName="+user+"&MyPassWord="+password+"&LogIn="+login+"&email="+email)
+    ((user,password,login="Iniciar%20Sesi%C3%B3n",email="")=>"MyUserName="+user+"&MyPassWord="+password+"&LogIn="+login+"&email="+email)
 :type==="setPost"?
     ((data,folder={},path,path_names,enriquecido=0)=>"proceso=hacerPost&contenido="+data+"&carpeta="+folder+"&path="+path+"&path_names="+path_names+"&enriquecido="+enriquecido)
 :type==="getPosts"?
@@ -17,11 +17,12 @@ type==="auth"?
 
 class api{
     constructor(url){this.url = url;}
-    call = (method="",{url=this.url,path="/",params=""},cookie=cookieJar)=>axios[method](
-            url+path,params,{
-            jar:cookie,
-            withCredentials: true
-        })
+    call = (method="",{url=this.url,path="/",params=""},cookie=false)=>axios[method](
+            url+path,
+            params,
+            // {jar:cookie,
+            // withCredentials: true}
+        )
     .then(({data,headers})=>({
         data: cheerio.load(data),
         headers,
@@ -30,12 +31,15 @@ class api{
     ))
     test = async (user,pass,cookie)=>{
         if(user&&pass&&cookie){
-            // let auth = tough.Cookie;
-            // if(typeof cookie === "object" && cookie.length){
-            //     cookie.map(e=>auth.parse(e))
-            // }
-            return this.call("post",{},getSI("auth")(user,pass)).then(a=>{
-                console.log(a);
+            let auth = tough.Cookie;
+            if(typeof cookie === "object" && cookie.length){
+                cookie.map(e=>auth.parse(e))
+            }
+            return this.call("post",{
+                path:"/init",
+                params:getSI("auth")(user,pass)
+            }).then(a=>{
+                console.log(a.cookie);
             });
         }else{
             return this.call("get",{}).then(({headers})=>headers["set-cookie"]?headers["set-cookie"]:[]);
