9ef32048f5509ffc67c54867489d11658eb7101a
thejavierCO <javier.cruz.odispo@hotmail.com>
2020-06-29 21:46:36 -0500

    day one
    creando una demo de funcionamiento principal

--- src/components/page.js
+++ src/components/page.js
@@ -1,4 +1,16 @@
 const { Router } = require('express')
 const app = Router();
 
+app.get("/unlogin",(req,res,next)=>{
+    res.render("territorium",{type:req.path.split("/")[1]});
+})
+
+app.get("/login",(req,res,next)=>{
+    res.render("territorium",{type:req.path.split("/")[1]});
+})
+
+app.get("/",(req,res,next)=>{
+    res.render("territorium");
+})
+
 module.exports = app;
--- src/public/main.js
+++ src/public/main.js
@@ -1,6 +1,69 @@
-let test = {
-    "init":"plasdyu",
-    "aijsodjas":"ajsoidjoias",
-    "asoidjias":"ajsdioas"
-}
+let getSession = ()=>fetch("/api/getSession")
+    .then(e=>e.json())
+    .then(e=>{
+        if(typeof e === "object"&&e.length){
+            sessionStorage.setItem("LoginData",JSON.stringify(e));
+            return {
+                status:true,
+                data:e,
+                log:"success"
+            }
+        }else{
+            throw {
+                status:false,
+                data:e,
+                error:"require object list"
+            }
+        }
+    })
 
+let setLogin = (user,pass,cookies)=>fetch("/api/getAuth",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({user,pass,cookies})
+    })
+    .then(e=>e.json())
+    .then(e=>{
+        let token = JSON.parse(sessionStorage.getItem("LoginData"));
+        if(typeof e === "object"&&e.length){
+            if(e.length === 1){
+                token[1] = e[0];
+                sessionStorage.setItem("LoginData",JSON.stringify(token))
+                return {
+                    status:true,
+                    data:token,
+                    log:"success"
+                };
+            }else{
+                sessionStorage.setItem("LoginData",JSON.stringify(e));
+                throw {
+                    status:false,
+                    data:e,
+                    error:"not login"
+                }
+            }
+        }else{
+            throw {
+                status:false,
+                data:e,
+                error:"require object list"
+            }
+        }
+    })
+
+document.addEventListener("DOMContentLoaded",()=>{
+    let token = JSON.parse(sessionStorage.getItem("LoginData"));
+    let login = document.querySelector("#login");
+    let user = document.querySelector("#user");
+    let pass = document.querySelector("#pass");
+    login.addEventListener("submit",(a)=>{
+        a.preventDefault();
+        setLogin(user.value,pass.value,token)
+        .then(e=>{
+            console.log(e);
+        })
+        .catch(e=>{
+            console.warn(e);
+        })
+    })
+})
