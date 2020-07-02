3b8c2813b1b4327a7393864ac3dd11c73dbca61b
thejavierCO <javier.cruz.odispo@hotmail.com>
2020-07-02 00:10:36 -0500

    reacreando 2

--- src/components/api.js
+++ src/components/api.js
@@ -5,52 +5,125 @@ const { Router } = require("express");
 const e = require("express");
 const app = Router();
 
-const test = false;
+const type = false;
 
-const tr = test!==false?
+const tr = type!==false?
 (new api("https://webhook.site/8fe70860-9770-41f4-8b13-89815c9e0852"))
 :(new api("https://conalep.territorio.la/"));
 
 let getdata = (html)=>{
-    let test = (a,b)=>a.filter(e=>{
-        let tags = b.split("|");
-        
-        // continua por aqui
-        // e.tag==="div"?true:(
-        //     e.tag==="p"?true:(
-        //         e.tag==="a"?true:(
-        //             e.tag==="small"?true:(
-        //                 e.tag==="input"?true:(
-        //                     e.tag==="button"?true:(
-        //                         e.tag==="input"?true:(
-        //                             e.node==="text"?(true):false
-        //                         )
-        //                     )
-        //                 )
-        //             )
-        //         )
-        //     )
-        // )
-    }
+    let gettags = test(html,"div|p|a|small|input|button|text")
+    let result = extract(gettags,["a","div","input","small","text"]
+    // ,({name,data})=>{
+    //     switch(name){
+    //         case "a":
+    //             console.log(data)
+    //             let { attr:{href}, child } = data;
+    //             data = href.split("perfil.php?").filter(e=>e===""||e==="#"?false:true);
+    //             data = data.map(e=>{
+    //                 e = e.split("=")
+    //                 e = e.length>1?e:(e[0])
+    //                 .split("javascript")
+    //                 .filter(e=>e===""||e==="#"?false:true)
+    //                 .map(e=>e.slice(e.indexOf(":")+1,e.indexOf(";")))
+    //                 let dt = {};
+    //                 if(e.length===2){
+    //                     dt["id"] = +e[1]?+e[1]:e[1];
+    //                     if(child.length === 1){
+    //                         child.map((a)=>{
+    //                             dt["dt"] = a.text?a.text.join(" "):a;
+    //                         });
+    //                     }else{
+    //                         dt["dt"] = a;
+    //                     }
+    //                     return dt;
+    //                 }else{
+    //                     dt = {
+    //                         f:e,
+    //                         dt:child.map((a)=>a.text?a.text.join(" "):a)
+    //                     }
+    //                     return dt;
+    //                 }
+    //             })
+    //             return data;
+    //         break;
+    //         case "div":
+    //             let {} = data;
+    //             // console.log(data.length)
+    //         break;
+    //     }
+    // }
     )
-    .map(e=>{
-        let {child,text} = e
-        if(child){
-            child = test(child);
-            e["child"] = child;
-            return e;
-        }else if(text){
-            e["text"] = text.replace(/( )/g,"");
-            return e;
-        }else{
-            return e;
-        }
-    })
-    return test(html,"div|p|a|small|input|button|text")
-    .map(e=>{
-        return tohtml(e);
-    });
+    return result;
 }
+
+let extract = (json,tags,f)=>{
+    if(typeof json === "object"&&json.length){
+        let result = [];
+        json.map((a)=>{
+            let {child,tag:name} = a;
+            tags.map(e=>{
+                if(e===name){
+                    if(child&&typeof child === "object"){
+                        extract(child,tags,f);
+                    }
+                    result.push(f?f({name,data:a}):{name,data:a});
+                }
+            })
+        })
+        return result;
+    }else if(typeof json !== "object"){
+        throw "error_check_extract"
+    }
+}
+let test = (a,b)=>a.filter(({tag,text})=>{
+    let result;
+    let tags = b.split("|");
+    for (let i = 0; i < tags.length; i++) {
+        if(tags[i]===tag)result = true;
+    }
+    if(text){
+        let result = [];
+        text = text.replace(/(\n)/g,"")
+        text.split(" ").map(e=>{
+            if(e!==""){
+                result.push(e);
+            }
+        })
+        if(result.length!==0){
+            return true
+        }else{
+            return false
+        }
+    }
+    if(typeof result!== undefined){
+        return true;
+    }
+    return false;
+}).map(e=>{
+    let {child,text} = e
+    if(child){
+        child = test(child,"div|p|a|small|input|button|text");
+        e["child"] = child;
+        return e;
+    }else if(text){
+        let result = [];
+        text = text.replace(/(\n)/g,"")
+        text.split(" ").map(e=>{
+            if(e!==""){
+                result.push(e);
+            }
+        })
+        if(result.length!==0){
+            e["text"] = result;
+        }else{
+            e["text"] = "";
+        }
+        return e;
+    }else{
+        return e;
+    }
+})
 
 app.get("/getSession",(req,res,next)=>{
     tr.test()
@@ -74,12 +147,19 @@ app.post("/getAuth",({body:{user,pass,cookies}},res,next)=>{
 
 app.post("/setAction",({body:{cookie,type,params}},res,next)=>{
     tr.set(type,params,cookie)
-    .then(({data})=>tojson(data("html").html()))
-    .then(e=>{
-        if(e["child"][1]["tag"] === "body"){
-            return e["child"][1]["child"];
+    .then(({data,headers})=>{
+        if(data("body").find("div").length!==0){
+            return tojson(data("html").html())
         }else{
-            throw {error:"require body"}
+            throw "not login"
+        }
+    })
+    .then(({child:tags})=>{
+        let {tag,child} = tags[1];
+        if(tag === "body"){
+            return child;
+        }else{
+            throw "require body"
         }
     })
     .then(e=>{
@@ -100,10 +180,19 @@ app.post("/setAction",({body:{cookie,type,params}},res,next)=>{
         }
         res.json(e);
     })
-    .then(e=>{
-        res.json(e)
+    // .then(e=>{
+    //     res.json(e)
+    // })
+    .then(e=>res.json({
+        status:true,
+        data:e
+    }))
+    .catch(e=>{
+        console.log(e);
+        res.json({
+            status:false,
+            error:e.toString()})
     })
-    .catch(e=>{res.json({error:e.toString()})})
 })
 
 module.exports = app;
